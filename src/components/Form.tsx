1cbc1f254e1b9bbcfdc4012be0cc5c6ed75248e4
WilliamNunes905 <williamtrybe@gmail.com>
2023-11-23 18:04:04 -0300

    Fazendo validação dos inputs e salvando o state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,67 @@
+import React, { useState } from "react"
 import Button from "./Button"
 
 export default function Form() {
+  const [formInfo, setFormInfo] = useState({
+    servico: '',
+    login: '',
+    senha: '',
+    url: ''
+  })
+  const [errorMessage, setErrorMessage] = useState<string[]>([]);
+  
+  function clearState() {
+    setFormInfo({
+      servico: '',
+      login: '',
+      senha: '',
+      url: ''
+    })
+  }
+
+  function validateForm() {
+    const errors = [];
+    const regex = /^(?=.*?[0-9])/i;
+
+    if (formInfo.servico === '') errors.push('O campo serviço é Obrigatorio');
+    if (formInfo.login === '') errors.push('O campo login é Obrigatorio');
+
+    if (!regex.test(formInfo.senha)) errors.push(
+      'O campo senha deve possuir letras maiusculas números e caracteres'
+      );
+    if (formInfo.senha === '') errors.push('O campo senha é Obrigatorio');
+    if (formInfo.url === '') errors.push('O campo URL é Obrigatorio');
+    setErrorMessage(errors);
+    return errors.length === 0;
+  }
+
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement> ) {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (validateForm()) {
+      setErrorMessage([]);
+      clearState();
+    }
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={ (event) => handleSubmit(event) } >
         <fieldset>
           <label>
             Nome do serviço
                 <input
+                  value={ formInfo.servico }
+                  name="servico"
+                  onChange={ (event) => handleChange(event) }
                   type="text"
                 />
           </label>
@@ -15,6 +69,9 @@ export default function Form() {
           <label>
             Login
                 <input
+                  value={ formInfo.login }
+                  name="login"
+                  onChange={ handleChange }
                   type="text"
                 />
           </label>
@@ -22,6 +79,10 @@ export default function Form() {
           <label>
             Senha
                 <input
+                  value={ formInfo.senha }
+                  name="senha"
+                  onChange={ handleChange }
+                  maxLength={ 20 }
                   type="password"
                 />
           </label>
@@ -29,12 +90,26 @@ export default function Form() {
           <label>
             URL
                 <input
+                  value={ formInfo.url }
+                  name="url"
+                  onChange={ handleChange }
                   type="text"
                 />
           </label>
 
+          {
+            errorMessage && (
+              <div className="invalid-password-check">
+                {errorMessage.map(message => (
+                  <p key={message} >{message}</p>
+                ))
+                }
+              </div>
+            )
+          }
           <Button>Cadastrar</Button>
-          <Button>Limpar</Button>
+          <Button onClick={ () => clearState() } >Limpar</Button>
+
         </fieldset>
       </form>
     </div>
